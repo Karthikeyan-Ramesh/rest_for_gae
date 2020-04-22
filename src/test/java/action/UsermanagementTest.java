d4edd9c5f08d7ffb3fd5039e9f514d718b7f4eb7
Karthikeyan-Ramesh <karthikeyan15061998@gmail.com>
2020-04-22 15:05:18 +0530

    Commited test case java file

---
+++ src/test/java/action/UsermanagementTest.java
@@ -0,0 +1,14 @@
+package action;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsermanagementTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
